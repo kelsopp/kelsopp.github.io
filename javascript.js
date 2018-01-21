952a578e84354078acbd33410faeec9c0dfed903
kelsey <kelsey.oppenheimer@gmail.com>
2018-01-21 15:04:53 -0500

    Added some shit

---
+++ javascript.js
@@ -0,0 +1,26 @@
+/*
+this is a comment
+*/
+
+// this is for a single line //
+
+console.log('Hello Wolrd');
+
+// alert('Hello World!');//
+
+document.write('Hello World!');
+
+
+var numberOfDogs = 5;
+var numberOfCats = 6;
+var numberofAnimals = numberOfDogs + numberOfCats;
+
+console.log(numberOfDogs);
+
+console.log(numberofAnimals);
+
+var kittensName = 'Kat';
+
+console.log(kittensName);
+
+console.log('The cats\' name' + 'is' + kittensName);
